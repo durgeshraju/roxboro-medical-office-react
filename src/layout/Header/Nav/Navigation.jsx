11e15c0de069e4ec0814c716b40a09e4c9957215
Durgesh <durgeshnidadavolu@gmail.com>
2026-01-26 16:14:39 +0800

    feat: routing setup done

--- src/layout/Header/Nav/Navigation.jsx
+++ src/layout/Header/Nav/Navigation.jsx
@@ -1,8 +1,13 @@
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 const Navigation = () => {
     return (
         <>
             <h1>Navigation</h1>
+            <Link to="/">Home</Link>
+            <Link to="/gallery">Gallery</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './styles/reset.css';
 import './styles/variables.css';
 import './styles/global.css';
@@ -8,6 +9,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+     </BrowserRouter>
   </StrictMode>,
 )
